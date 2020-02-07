c3b752e75e736428be892f34609abf08fcf846f2
Your Name <y.dream.nest@icloud.com>
2020-02-07 16:14:48 +0900

    Styling top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/about'
-  get 'static_pages/contact'
-  get 'static_pages/terms'
-  get 'static_pages/policy'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/terms', to: 'static_pages#terms'
+  get '/policy', to: 'static_pages#policy'
 end
