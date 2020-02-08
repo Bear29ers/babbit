572e2925a357b4ec675d66a98d27fe64d68a105f
Your Name <y.dream.nest@icloud.com>
2020-02-08 16:40:20 +0900

    Finish user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "ようこそ Loca!!yへ！！ アカウントが作成されました。"
+      redirect_to @user
     else
       render 'new'
     end
