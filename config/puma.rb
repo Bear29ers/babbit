786908e0035785e21419c4c1c47e9085929c2579
Your Name <y.dream.nest@icloud.com>
2020-02-08 16:55:41 +0900

    Revise Puma setting

--- config/puma.rb
+++ config/puma.rb
@@ -4,18 +4,8 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-threads threads_count, threads_count
 
-rackup DefaultRackup
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-port        ENV.fetch("PORT") { 3000 }
-
-# Specifies the `environment` that Puma will run in.
-#
-environment ENV.['RACK_ENV'] || 'development'
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
@@ -23,6 +13,8 @@ environment ENV.['RACK_ENV'] || 'development'
 # processes).
 #
 workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+threads_count = Integer(ENV["RAILS_MAX_THREADS"] || 5)
+threads threads_count, threads_count
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
@@ -33,6 +25,15 @@ workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 #
 preload_app!
 
+rackup DefaultRackup
+
+# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+#
+port        ENV['PORT'] || 3000
+
+# Specifies the `environment` that Puma will run in.
+#
+environment ENV.['RACK_ENV'] || 'development'
 # If you are preloading your application and using Active Record, it's
 # recommended that you close any connections to the database before workers
 # are forked to prevent connection leakage.
