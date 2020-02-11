18725ae04a8607857044dcc6a011d3130b24d803
Your Name <y.dream.nest@icloud.com>
2020-02-11 17:35:06 +0900

    Add config.action_mailer.raise_delivery_errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "locally_#{Rails.env}"
   config.action_mailer.perform_caching = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'locally-review.herokuapp.com'
   config.action_mailer.default_url_options = {host: host}
