534ae00711f133251c33a3e34746e8f691cc8214
Your Name <y.dream.nest@icloud.com>
2020-02-13 01:45:24 +0900

    Implement mailing for password reset

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -13,4 +13,16 @@ class UserMailerTest < ActionMailer::TestCase
     assert_match CGI.escape(user.email), mail.text_part.body.to_s.encode("UTF-8")
   end
 
+  test "password_reset" do
+    user = users(:test_user1)
+    user.reset_token = User.new_token
+    mail = UserMailer.password_reset(user)
+    assert_equal "パスワードの再設定 / Loca!!y", mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal ["noreply@example.com"], mail.from
+    assert_match user.name, mail.text_part.body.to_s.encode("UTF-8")
+    assert_match user.reset_token, mail.text_part.body.to_s.encode("UTF-8")
+    assert_match CGI.escape(user.email), mail.text_part.body.to_s.encode("UTF-8")
+  end
+
 end
