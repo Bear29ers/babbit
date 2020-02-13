92b14f2c4b52f1bb3d6ce54279d824f5080ce057
Your Name <y.dream.nest@icloud.com>
2020-02-13 22:00:42 +0900

    Post validation and its test

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:test_user1)
+    #このコードは慣習的に正しくない
+    @post = Post.new(content: "Lorem ipsum", user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @post.valid?
+  end
+
+  test "user id should be present" do
+    @post.user_id = nil
+    assert_not @post.valid?
+  end
+
+  test "content should be present" do
+    @post.content = "  "
+    assert_not @post.valid?
+  end
+
+  test "content should be at most 500 characters" do
+    @post.content = "a" * 501
+    assert_not @post.valid?
+  end
 end
