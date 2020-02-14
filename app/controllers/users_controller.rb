39b3faa3b66711c717d3b49c3490f4b0545001c6
Your Name <y.dream.nest@icloud.com>
2020-02-14 17:51:45 +0900

    Shift logged_in_user method to application_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,15 +54,6 @@ class UsersController < ApplicationController
     end
 
     #beforeアクション
-    #ログイン済みのユーザーかどうかを確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "このページにアクセスするにはログインしてください"
-        redirect_to login_url
-      end
-    end
-
     #正しいユーザーかどうかを確認
     def correct_user
       @user = User.find(params[:id])
