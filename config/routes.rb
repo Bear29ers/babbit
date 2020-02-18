72d6fa0c9c530fa7afc6246b3985acd3eb4cf482
Your Name <y.dream.nest@icloud.com>
2020-02-18 14:44:55 +0900

    Fixed contact routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
   root 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/terms', to: 'static_pages#terms'
@@ -19,6 +15,7 @@ Rails.application.routes.draw do
   post '/newpost', to: 'posts#create'
 
   get '/contact', to: 'contacts#new'
+  post '/contact', to: 'contacts#create'
 
   resources :users
   resources :account_activations, only: [:edit]
