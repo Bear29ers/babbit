cf3f15ff94503b515a32aca3d469f86318c8d8f4
Your Name <y.dream.nest@icloud.com>
2020-02-20 23:31:01 +0900

    Add ststs partial

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,11 @@ Rails.application.routes.draw do
   get '/contact', to: 'contacts#new'
   post '/contact', to: 'contacts#create'
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :posts
