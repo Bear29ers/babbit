e31892b84cf543f8418e56a84b6e8903da5413e2
Your Name <y.dream.nest@icloud.com>
2020-02-25 21:38:42 +0900

    Styled thumbs up button in the post show page

--- test/models/good_test.rb
+++ test/models/good_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class GoodTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @good = Good.new(user_id: users(:test_user1).id, post_id: posts(:test_post4).id)
+  end
+
+  test "should be valid" do
+    assert @good.valid?
+  end
+
+  test "should require a user id" do
+    @good.user_id = nil
+    assert_not @good.valid?
+  end
+
+  test "should require a post id" do
+    @good.post_id = nil
+    assert_not @good.valid?
+  end
 end
