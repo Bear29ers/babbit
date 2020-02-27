a891d9de81a2c87ab5e0e13eeeb9a26e08a03153
Your Name <y.dream.nest@icloud.com>
2020-02-28 00:27:33 +0900

    Complete thumbs buttons with Ajax

--- test/integration/thumbsupdown_test.rb
+++ test/integration/thumbsupdown_test.rb
@@ -15,13 +15,39 @@ class ThumbsupTest < ActionDispatch::IntegrationTest
     assert_difference '@post.goods.count', 1 do
       post post_goods_path(@post)
     end
-    assert_redirected_to root_url
+    # good = @post.goods.find_by(user_id: @user.id, post_id: @post.id)
+    # assert_difference '@post.goods.count', -1 do
+    #   delete post_goods_path(@post, good)
+    # end
+  end
+
+  test "should thumbs up a post with Ajax" do
+    assert_difference '@post.goods.count', 1 do
+      post post_goods_path(@post), xhr: true
+    end
+    # good = @post.goods.find_by(user_id: @user.id, post_id: @post.id)
+    # assert_difference '@post.goods.count', -1 do
+    #   delete post_goods_path(@post, good), xhr: true
+    # end
   end
 
   test "should thumbs down a post the standard way" do
     assert_difference '@post.bads.count', 1 do
       post post_bads_path(@post)
     end
-    assert_redirected_to root_url
+    # bad = @post.bads.find_by(user_id: @user.id, post_id: @post.id)
+    # assert_difference '@post.bads.count', -1 do
+    #   delete post_bads_path(bad)
+    # end
+  end
+
+  test "should thumbs down a post with Ajax" do
+    assert_difference '@post.bads.count', 1 do
+      post post_bads_path(@post), xhr: true
+    end
+    # bad = @post.bads.find_by(user_id: @user.id, post_id: @post.id)
+    # assert_difference '@post.bads.count', -1 do
+    #   delete post_bads_path(bad), xhr: true
+    # end
   end
 end
