972997a9dd9184451d1be0426af2e39d11d368d3
Your Name <y.dream.nest@icloud.com>
2020-03-18 17:47:32 +0900

    Add habit in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ User.create!(
             login_at: Time.zone.now
             )
 
-40.times do |n|
+30.times do |n|
   name = Faker::Name.name
   email = "exmaple-#{n+1}@railstutorial.org"
   password = "password"
@@ -28,7 +28,7 @@ end
 # 投稿
 users = User.order(:created_at).take(30)
 3.times do
-  # content = Faker::Lorem.paragraphs(supplemental: true)
+  habit = "たばこや食生活の乱れ、不規則な睡眠時間など"
   content = "悪い習慣を辞めるためには、まずは、「なぜその習慣を断つ必要があるのか」をできるかぎり明確にしましょう。「本気で改善しないといけない」と自分自身に思い込ませるためです。「なんとなくやめたい」では絶対に続きません。悪い習慣を断とうという挑戦をしていくうえで、「自己否定的な考えは捨てる」ことも重要です。特に自己否定をする傾向にある人は要注意です。「自分は続かない」といった自己否定的な考えが、悪い習慣を断とうという試みを妨害し、元鞘に戻るための言い訳のように作用するのです。"
   users.each {|user| user.posts.create!(content: content)}
 end
@@ -36,15 +36,15 @@ end
 # リレーションシップ
 users = User.all
 user = users.first
-following = users[2..40]
-followers = users[3..30]
+following = users[2..30]
+followers = users[3..25]
 following.each { |followed| user.follow(followed) }
 followers.each { |follower| follower.follow(user) }
 
 # Good、Bad
 posts = Post.all
 users = User.all
-nonadmin = users[35..40]
+nonadmin = users[25..30]
 posts.each do |post|
   nonadmin.each do |user|
     user.goods.create!(post_id: post.id)
